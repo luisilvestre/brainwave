c36d7012bb33180a1dfc11f0049c65955c63511b
luisilvestre <luis_productions@hotmail.com>
2024-04-18 18:30:05 +0100

    V0.6

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:-[5.25rem] overflow-hidden">
-        <Header/>
-        <Hero/>
-        <Benefits/>
-        <Collaboration/>
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
